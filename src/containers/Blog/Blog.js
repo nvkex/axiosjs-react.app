544c57551e2d7db14b2830c03de0745893912daa
nvkex <scrbwrm@gmail.com>
2020-07-11 22:33:38 +0530

    404 errors displayed

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,41 +11,49 @@ class Blog extends Component {
     state = {
         posts: [],
         selectedPostId: null,
+        error: false
     }
 
-    componentDidMount(){
+    componentDidMount() {
         Axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
                 console.log(response);
-                const posts = response.data.slice(0,4);
+                const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
-                    return {...post, author: 'nvkex'};
+                    return { ...post, author: 'nvkex' };
                 });
-                this.setState({posts: updatedPosts});
+                this.setState({ posts: updatedPosts });
+            })
+            .catch(error => {
+                this.setState({ error: true });
             });
-        
+
     }
 
-    postSelectedHandler(id){
-        this.setState({selectedPostId:id});
+    postSelectedHandler(id) {
+        this.setState({ selectedPostId: id });
     }
 
-    render () {
+    render() {
 
-        const posts = this.state.posts.map( (post) => {
-            return <Post 
-                key = {post.id} 
-                title = {post.title} 
-                author = {post.author}
-                clicked = {() => this.postSelectedHandler(post.id)}/>
-        })
+        let posts = <p style={{ textAlign: 'center', color:'red' }}>Something went wrong!!</p>
+
+        if (!this.state.error) {
+            posts = this.state.posts.map((post) => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost id = {this.state.selectedPostId} />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
