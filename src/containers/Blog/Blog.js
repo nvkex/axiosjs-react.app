8d1ef0deb680ff48406b331f20c9013e8df457f9
nvkex <scrbwrm@gmail.com>
2020-07-16 12:51:18 +0530

    relative path

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,7 +14,11 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><Link to="/">Home</Link></li>
-                            <li><Link to="/new-post">New Post</Link></li>
+                            <li><Link to={{
+                                pathname: this.props.match.url + '/new-post',
+                                hash: '#submit',
+                                search:'?q=hello'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>    
