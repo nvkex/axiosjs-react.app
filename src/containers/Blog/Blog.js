c8c8bae582fa3c4d16e8bf9a3fa251b30046b429
nvkex <scrbwrm@gmail.com>
2020-07-16 12:45:57 +0530

    withRouter components:

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
 import './Blog.css';
 // import Axios from 'axios';
-import Posts from './Posts/Post';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -10,12 +13,13 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href ="/">Home</a></li>
-                            <li><a href ="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>    
-                <Posts/>            
+                <Route path = "/" exact component={Posts} />
+                <Route path = "/new-post" component={NewPost} />            
             </div>
         );
     }
